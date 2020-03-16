0efa61e1baee3a72714c1219ab96b46ce511057f
Itai Levi <itailevi420@gmail.com>
2020-03-16 12:50:50 +0200

    fixes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 // const fs = require('fs');
 const port = 3001;
 const cors = require('cors');
-const knex = require('knex');
 const bcrypt = require('bcryptjs');
+const register = require('./controller/register')
+const signin = require('./controller/signin')
+const profile = require('./controller/profile')
+const image = require('./controller/image')
 
-
+const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
@@ -29,26 +32,9 @@ app.use(cors());
 // app.get('/', (req, res) => {
 //   res.send(database.users);
 // })
-
-app.post('/signin', (req, res) => {
-  const { email, password } = req.body; 
-  db.select('email', 'hash').from('login')
-  .where('email', '=', email)
-  .then(data => {
-    // console.log(data)
-    const isValid = bcrypt.compareSync(password, data[0].hash);
-    // console.log(isValid)
-    if(isValid) {
-     db.select('*').from('users')
-        .where('email', '=', email)
-        .then(user => res.json(user[0]))
-        .catch(err => res.status(400).json('unable to get user'))
-    } else{
-       throw(err);
-    }
-  })
-  .catch(err => res.status(400).json('email or password does not exist'))
-})
+// we pass bcrypt & db to the function via dependence injection like so. 
+// instead of importing them in function file (although that would work too).
+app.post('/signin', (req, res) =>  signin.handleSignin(req, res, bcrypt, db))
 
 /*
 req e.g {
@@ -57,37 +43,9 @@ req e.g {
   password: '',
 }
 */
-
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-
-    return trx('users')
-        .returning('*')
-        .insert({
-          name: name,
-          email: loginEmail[0],
-          joined: new Date()
-          
-        })
-        .then(user=> res.json(user[0]))
-      })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err => res.status(400).json('Something Went Wrong, can not register!!!'))
-})
-
-
+// we pass bcrypt & db to the function via dependence injection like so. 
+// instead of importing them in function file (although that would work too).
+app.post('/register', (req, res) => register.handleRegister(req, res, bcrypt, db )) 
 /* One way to use  Knex ".where" method is with an object e.g:
 .where({
   id: id,
@@ -95,33 +53,16 @@ app.post('/register', (req, res) => {
 in ES6 syntax if the key and value are the same  so you can do it like this as well: (assume obj like example above)
 .where({id})
 */
-app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
-  db.select('*')
-    .from('users')
-    .where({id})
-    .then(user => {
-      if(user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json('not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))  
-})
+//  Another way of calling the function is like so:
+app.get('/profile/:id', profile.profileHandlerGet(db));
 
-app.put('/image', (req, res) => {
-  const {id} = req.body;
-  db('users')
-    .where('id' ,'=' , id)
-    .increment('entries' , 1)
-    .returning('entries')
-    .then(ent => res.json(ent[0]))
-    .catch(err => res.status(400).json("can't get entries"))
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
-})
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`)
-})
+});
+
+
 
